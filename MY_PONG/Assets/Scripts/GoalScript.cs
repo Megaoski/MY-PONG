609c8499a442077a171212c5f583372e7aec8645
Megaoski <oscar_2596@hotmail.com>
2021-07-29 22:17:41 +0200

    Basic gameplay
    Collisions working, ball reset working, score working

---
+++ MY_PONG/Assets/Scripts/GoalScript.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalScript : MonoBehaviour
+{
+    public static int score1;
+    public static int score2;
+    public bool Porteria1;
+    public bool Porteria2;
+    public static bool Player1HasScored = false;
+    public static bool Player2HasScored = false;
+
+    Ball ball;
+    
+    void Start()
+    {
+        score1 = 0;
+        score2 = 0;
+
+        ball = GameObject.FindGameObjectWithTag("Ball").GetComponent<Ball>();//to call and reset the ball
+
+    }
+    void Update()
+    {
+        if (Player1HasScored)
+        {
+            score1 += 1;
+            Player1HasScored = false;
+        }
+
+        if (Player2HasScored)
+        {
+            score2 += 1;
+            Player2HasScored = false;
+        }
+
+        Debug.Log(score1);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ball"))
+        {
+            if (Porteria2)
+            {
+                Player1HasScored = true;
+                Debug.Log("Player 1 Scored");
+                ball.Reset();
+            }
+            else
+            {
+                Player2HasScored = true;
+                Debug.Log("Player 2 Scored");
+                ball.Reset();
+            }
+           
+        }
+
+        
+    }
+
+
+    
+}
---
+++ MY_PONG/Assets/Scripts/Score.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    GameObject canvasObject;
+    Transform score1Tr;
+    Text score1Text;
+    Transform score2Tr;
+    Text score2Text;
+
+    public int Score1;
+    public int Score2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        canvasObject = GameObject.Find("Canvas");
+        score1Tr = canvasObject.transform.Find("Score1");
+        score1Text = score1Tr.GetComponent<Text>();
+        score2Tr = canvasObject.transform.Find("Score2");
+        score2Text = score2Tr.GetComponent<Text>();
+
+        //Score1 = GoalScript.score1;
+        //Score2 = GoalScript.score2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Score1 = GoalScript.score1;
+        Score2 = GoalScript.score2;
+
+        score1Text.text = Score1.ToString();
+        score2Text.text = Score2.ToString();
+
+        //Debug.Log("" + Score1);
+    }
+}
